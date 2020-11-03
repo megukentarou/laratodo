8457f2d96b9b0eeea8bdb80436ba630a4ac9f982
megukentarou <megukentarou@gmail.com>
2020-11-03 15:36:57 +0900

    Wordモデルのindexにデータ反映

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Word;
+use Illuminate\Support\Facades\DB;
 
 class WordController extends Controller
 {
     public function index()
     {
-        return view('words.index');
+        $values = Word::all();
+
+        $words = DB::table('words')
+        ->select('id', 'text', 'impression', 'action')
+        ->get();
+
+        // dd($words);
+     
+        return view('words.index', compact('values'));
     }
 }
--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -1,1 +1,7 @@
-wordモデルのindex”
+wordモデルのindex
+<br>
+@foreach($values as $value)
+{{$value->text}}<br>
+{{$value->impression}}<br>
+{{$value->action}}<br>
+@endforeach
