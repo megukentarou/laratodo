1fdda8d6d87c01945c955b62a868a3053c9490b2
megukentarou <megukentarou@gmail.com>
2020-11-04 09:53:18 +0900

    dreate index.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    home.bladeです
                 </div>
             </div>
         </div>
--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -1,7 +1,24 @@
 wordモデルのindex
-<br>
-@foreach($values as $value)
-{{$value->text}}<br>
-{{$value->impression}}<br>
-{{$value->action}}<br>
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    ログインしました
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
