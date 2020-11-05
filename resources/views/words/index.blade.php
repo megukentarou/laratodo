35192ec704cdc1fe7e738729bb29f8c59ea0327c
megukentarou <megukentarou@gmail.com>
2020-11-05 16:11:26 +0900

    ホーム画面のコメント実装

--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -11,7 +11,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">投稿一覧</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -41,8 +41,6 @@
                     <thead class="thead-dark">
                       <tr>
                         <th scope="col">id</th>
-                        <!-- <th scope="col">テキスト</th>
-                        <th scope="col">感想</th> -->
                         <th scope="col">行動</th>
                         <th scope="col">詳細</th>
                       </tr>
@@ -51,8 +49,6 @@
                     @foreach($words as $word)
                     <tr>
                     <th scope="row">{{ $word->id}}</th>
-                    <!-- <td>{{ $word->text}}</td>
-                    <td>{{ $word->impression}}</td> -->
                     <td>{{ $word->action}}</td>
                     <td><a href="{{ route('words.show', ['id' => $word->id]) }}" class="btn btn-light" >詳細を見る</a></td>
                     @endforeach  
--- resources/views/words/show.blade.php
+++ resources/views/words/show.blade.php
@@ -4,32 +4,30 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">
+                    詳細内容
+                </div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                   <br>
-                   ・印象に残った言葉
-                   <br>
-                    {{ $word->text }}
-                    <br>
-                    ・どの様な印象を持ったか
-                    <br>
-                    {{ $word->impression }}
-                    <br>
-                    ・具体的にどの様な行動をするのか
-                    <br>
-                    {{ $word->action }}
-                    <br>
+                    <h5 class="card-title">印象に残った言葉</h5>
+                    <p class="card-text">
+                    {{ $word->text }}</p>
+                    <h5 class="card-title">どの様な印象を持ったか</h5>
+                    <p class="card-text">
+                    {{ $word->impression }}</p>
+                    <h5 class="card-title">具体的にどの様な行動をするのか</h5>
+                    <p class="card-text">
+                    {{ $word->action }}</p>
                     <form method="GET" action="{{ route('words.edit', ['id' => $word->id]) }}">
                     @csrf
                     <input class="btn btn-primary" type="submit" value="編集する">              
                     </form>
+                    <br>
+                    <a href="#">いいねする（実装中）</a>
                 </div>
             </div>
         </div>
